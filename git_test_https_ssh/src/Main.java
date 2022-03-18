ba40dc2a4def6f85166e1a56bf0990484a490530
偷塔者 <59313911+Ningwencheng@users.noreply.github.com>
2022-03-18 12:15:54 +0800

    Update Main.java

--- git_test_https_ssh/src/Main.java
+++ git_test_https_ssh/src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int a;
         int b;
+        int c;
         System.out.println("hot-fix test");
         System.out.println("==================");
         System.out.println("这次会出现pull冲突的问题");
