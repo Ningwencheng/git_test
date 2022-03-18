57aadc279b1dec6d3b0d8d5f0dce34c49c508970
TouTazhe <2776199128@qq.com>
2022-03-18 12:12:52 +0800

    pull configs

--- git_test_https_ssh/src/Main.java
+++ git_test_https_ssh/src/Main.java
@@ -1,14 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test======================================");
-        System.out.println("IDEA first commit!");
-        System.out.println("IDEA second commit");
-        System.out.println("IDEA last commit");
-        System.out.println("=======================");
-        System.out.println("hot-fix branch commit");
-        System.out.println("master test");
+        int a;
+        int b;
         System.out.println("hot-fix test");
-        System.out.println("push");
-        System.out.println("这次不会出现pull冲突的问题");
+        System.out.println("==================");
+        System.out.println("这次会出现pull冲突的问题");
+        System.out.println("这次会出现pull冲突的问题");
+        System.out.println("这次会出现pull冲突的问题");
+        System.out.println("这次会出现pull冲突的问题");
+        System.out.println("这次会出现pull冲突的问题");
+        System.out.println("这次会出现pull冲突的问题");
+        System.out.println("这次会出现pull冲突的问题");
+        System.out.println("这次会出现pull冲突的问题");
     }
 }
