8475e631f790aa1b0ac9a9e173a0e76544752745
TouTazhe <2776199128@qq.com>
2022-03-18 10:54:15 +0800

    hot-fix branch first commit

--- git_test_https_ssh/src/Main.java
+++ git_test_https_ssh/src/Main.java
@@ -4,5 +4,7 @@ public class Main {
         System.out.println("IDEA first commit!");
         System.out.println("IDEA second commit");
         System.out.println("IDEA last commit");
+        System.out.println("=======================");
+        System.out.println("hot-fix branch commit");
     }
 }
